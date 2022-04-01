a0aa09208e41d3efe079ccdf6e39bfcfc3896306
zhanghaofeng <zhanghaofeng@didiglobal.com>
2022-04-01 11:18:35 +0800

    rebuilding site 2022年 4月 1日 星期五 11时18分35秒 CST

--- deploy.sh
+++ deploy.sh
@@ -19,10 +19,7 @@ git push origin main
 
 cd ..
 
-# rm -rf hugo-blog-master
 mkdir -p hugo-blog-master
-
-# git clone git@github.com:zhangfinn/zhangfinn.github.io.git hugo-blog-master
 
 cd hugo-blog-master
 
@@ -34,9 +31,7 @@ git remote add origin	git@github.com:zhangfinn/zhangfinn.github.io.git
 # Add changes to git.
 git add .
 
-
 git commit -m "$msg"
-
 
 # Push source and build repos.
 git push origin master --force
