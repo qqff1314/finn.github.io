6d65a7faeb74d6820dd78a41394f656709a361ca
zhanghaofeng <zhanghaofeng@didiglobal.com>
2022-04-01 10:36:00 +0800

    rebuilding site 2022年 4月 1日 星期五 10时36分00秒 CST

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-
+MASTER_DIR  = hugo-blog-master
 # If a command fails then the deploy stops
 set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+rm -rf $MASTER_DIR
 
 git add .
 
@@ -22,9 +24,9 @@ hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 cd ..
 
-git clone git@github.com:zhangfinn/zhangfinn.github.io.git hugo-blog-master
+git clone git@github.com:zhangfinn/zhangfinn.github.io.git $MASTER_DIR
 
-cd hugo-blog-master
+cd $MASTER_DIR
 
 cp "../hugo-blog/public" ./
 
@@ -37,4 +39,6 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
+
+rm -rf $MASTER_DIR
 
